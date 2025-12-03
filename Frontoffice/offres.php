a04dbdbd7d2b8f2ec8b199e8cd661c4d7e9849ad
nassursio <nafayadhui.sio@gmail.com>
2025-12-03 11:57:04 +0300

    ajout d'offre

--- Frontoffice/offres.php
+++ Frontoffice/offres.php
@@ -11,19 +11,19 @@
     <h1>Offres d'emploi & stages</h1>
    
     <form action="offres.php" method="post">
-        <input type="text" name="Mot-clé" >
-        <input type="text" name="Type" >
-        <select id="type" name="type">
+        <input type="text" name="Mot-clé" placeholder="Mot-clé" >
+        <select id="type" name="type" placeholder="Type">
         <option value=""></option>
         </select>
-        <select id="ville" name="ville">
+        <select id="ville" name="ville" placeholder="ville" >
         <option value=""></option>
         </select>
-        <select id="télétravail" name="télétravail">
-        <option value=""></option>  
-
-        <button type="submit">Filtrer</button>
-        <button type="reset">Réinitialiser</button>
+        <select id="télétravail" name="télétravail" placeholder="télétravail"> >
+        <option value=""></option> 
+        <br>
+        <br>
+        <input type="submit"></button>
+        <input type="reset"></button>
 
 </body>
 </html>
