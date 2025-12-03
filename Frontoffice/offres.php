029654079fe9a181014a9bb182b726166d58f1c6
nassursio <nafayadhui.sio@gmail.com>
2025-12-03 11:50:06 +0300

    debut d'offre

--- Frontoffice/offres.php
+++ Frontoffice/offres.php
@@ -1,1 +1,29 @@
 <?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Offre</title>
+</head>
+<body>
+    <h1>Offres d'emploi & stages</h1>
+   
+    <form action="offres.php" method="post">
+        <input type="text" name="Mot-clé" >
+        <input type="text" name="Type" >
+        <select id="type" name="type">
+        <option value=""></option>
+        </select>
+        <select id="ville" name="ville">
+        <option value=""></option>
+        </select>
+        <select id="télétravail" name="télétravail">
+        <option value=""></option>  
+
+        <button type="submit">Filtrer</button>
+        <button type="reset">Réinitialiser</button>
+
+</body>
+</html>
