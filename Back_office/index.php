4dabf5d61009558991e9f502be33315b40cefae6
nassursio <nafayadhui.sio@gmail.com>
2025-12-03 10:47:15 +0300

    supretion de home

--- Back_office/index.php
+++ Back_office/index.php
@@ -1,11 +1,11 @@
-<?php
-$titrePage = "Tableau de bord";
-require 'entete.php';
-?>
-<h2>Tableau </h2>
-<p>Choisissez une section à gérer :</p>
-<ul>
-   
-
-</ul>
-<?php require 'pied.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tableau de bord</title>
+</head>
+<body>
+    
+</body>
+</html>
